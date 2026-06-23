5016f6df003ea82711dd71480a297fc3ac3a5dcb
moisesPomilio <93723302+moisesPompilio@users.noreply.github.com>
2026-06-23 10:32:55 -0300

    feat(proc-macro): add floresta-proc-macro crate for procedural macros
    Add a new crate `floresta-proc-macro`
    that provides the proc-macro utilities used across the project

---
+++ crates/floresta-proc-macro/src/lib.rs
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+// cargo docs customization
+#![cfg_attr(docsrs, feature(doc_cfg))]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/249173822")]
+#![doc(
+    html_favicon_url = "https://raw.githubusercontent.com/getfloresta/floresta-media/master/logo_png/Icon-Green(main).png"
+)]
+#![allow(clippy::manual_is_multiple_of)]
+#![cfg_attr(not(test), deny(clippy::as_conversions))]
